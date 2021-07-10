b5a757698fadc09e1265c58476c1e1c506475d93
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-07-10 15:00:11 +0900

    Add: 警告を追加

--- index.js
+++ index.js
@@ -1,72 +1,76 @@
 let language = window.navigator.userLanguage || window.navigator.language;
-if (language == "ja") {
+// if (language == "ja") {
 
-    PLACEHOLDER = `
+PLACEHOLDER = `
 import discord
-
 
 client = discord.Client()
 
+
 @bot.event
 async def on_ready():
-  print(f"Logged in as {client.user}")
+    print(f"Logged in as {client.user}")
+
 
 bot.run("Th1sIsN0t4T0k3n.B3cause.1fiSh0w1tB0tWillG3tH4cked")
+
 `.trim()
-    RESULT_PLACEHOLDER = `
-discord.pyが動かない原因をコードから推測するWebツールです。
+RESULT_PLACEHOLDER = `
+discord.pyを使ったコードが動かない原因をコードから推測するWebツールです。
 
 上のテキストボックスにコードを入力すると推測されます。
 
 コントリビューション大歓迎！
 https://github.com/sevenc-nanashi/dpy-error-guesser
 `.trim()
-    ERRORS = [
-        [/client=commands\.Bot/g, "?commands.Botはbotという名前の変数に入れることが一般的です。"],
-        [/@([a-z_A-Z]+)\.event\ndef (.+):/g, "@$1.eventに登録する関数は非同期（async def）である必要があります。"],
-        [/@([a-z_A-Z]+)\.event\n(?:async )?def (?!on_)[a-z_]+\(.*\):/g, "@$1.eventに登録する関数はon_から始まる必要があります。"],
-        [/@(?:[a-z_A-Z]+)\.event\nasync def on_([a-z_]+)\(.*\):[\s\S]+@(?:[a-z_A-Z]+)\.event\nasync def on_\1\(.*\)/g, "イベント on_$1が重複しています。最後のon_$1だけが実行されます。"],
-    ]
-    couldNotFind = "問題を検出できませんでした。"
-} else {
-    PLACEHOLDER = `
-import discord
+ERRORS = [
+    // [/client=commands\.Bot/g, "?commands.Botはbotという名前の変数に入れることが一般的です。"],
+    [/commands\.Bot[\s\S]*discord\.Client|discord\.Client[\s\S]*commands\.Bot/g, "clientはbotに置き換えることができます。"],
+    [/(?<=(.+)=commands\.Bot[\s\S]*)@\1.event\nasync def on_message(?![\s\S]*process_commands)/g, "$?コマンドを実行するにはprocess_commandsが必要です。"],
+    [/@([a-z_A-Z]+)\.event\ndef (.+):/g, "@$1.eventに登録する関数は非同期（async def）である必要があります。"],
+    [/@([a-z_A-Z]+)\.event\n(?:async )?def (?!on_)[a-z_]+\(.*\):/g, "@$1.eventに登録する関数はon_から始まる必要があります。"],
+    [/@(?:[a-z_A-Z]+)\.event\nasync def on_([a-z_]+)\(.*\):[\s\S]+@(?:[a-z_A-Z]+)\.event\nasync def on_\1\(.*\)/g, "イベント on_$1が重複しています。最後のon_$1だけが実行されます。"],
+]
+couldNotFind = "問題を検出できませんでした。"
+// } else {
+//     PLACEHOLDER = `
+// import discord
 
 
-client = discord.Client()
+// client = discord.Client()
 
-@bot.event
-async def on_ready():
-  print(f"Logged in as {client.user}")
+// @bot.event
+// async def on_ready():
+//   print(f"Logged in as {client.user}")
 
-bot.run("Th1sIsN0t4T0k3n.B3cause.1fiSh0w1tB0tWillG3tH4cked")
-`.trim()
-    RESULT_PLACEHOLDER = `
-A web tool that guesses the wrong code from source code.
+// bot.run("Th1sIsN0t4T0k3n.B3cause.1fiSh0w1tB0tWillG3tH4cked")
+// `.trim()
+//     RESULT_PLACEHOLDER = `
+// A web tool that guesses the wrong code from source code.
 
-Input textarea above to guess error.
+// Input textarea above to guess error.
 
-We welcome contributions!
-https://github.com/sevenc-nanashi/dpy-error-guesser
-`.trim()
-    ERRORS = [
-        [/client=commands\.Bot/g, "commands.Bot is usually placed in a variable named bot."],
-        [/@([a-z_A-Z]+)\.event\ndef (.+):/g, "Function for @$1.event should be async function (async def)."],
-        [/@([a-z_A-Z]+)\.event\n(?:async )?def (?!on_)[a-z_]+\(.*\):/g, "Name of function for @$1.event should be started with \"on_\"."],
-        [/@(?:[a-z_A-Z]+)\.event\nasync def on_([a-z_]+)\(.*\):[\s\S]+@(?:[a-z_A-Z]+)\.event\nasync def on_\1\(.*\)/g, "There're multiple event listener for on_$1, Last on_$1 will be called."],
-    ]
-    couldNotFind = "We couldn't find any problem."
+// We welcome contributions!
+// https://github.com/sevenc-nanashi/dpy-error-guesser
+// `.trim()
+//     ERRORS = [
+//         [/client=commands\.Bot/g, "commands.Bot is usually placed in a variable named bot."],
+//         [/@([a-z_A-Z]+)\.event\ndef (.+):/g, "Function for @$1.event should be async function (async def)."],
+//         [/@([a-z_A-Z]+)\.event\n(?:async )?def (?!on_)[a-z_]+\(.*\):/g, "Name of function for @$1.event should be started with \"on_\"."],
+//         [/@(?:[a-z_A-Z]+)\.event\nasync def on_([a-z_]+)\(.*\):[\s\S]+@(?:[a-z_A-Z]+)\.event\nasync def on_\1\(.*\)/g, "There're multiple event listener for on_$1, Last on_$1 will be called."],
+//     ]
+//     couldNotFind = "We couldn't find any problem."
 
-    replaces = {
-        "#input-container h2": "Input",
-        "label[for='main-code-textarea']": "Source code",
-        "#result-container h2": "Result",
-        "label[for='result-code-textarea']": "Source code"
-    }
-    for ([selector, text] of Object.entries(replaces)) {
-        document.querySelector(selector).innerText = text
-    }
-}
+//     replaces = {
+//         "#input-container h2": "Input",
+//         "label[for='main-code-textarea']": "Source code",
+//         "#result-container h2": "Result",
+//         "label[for='result-code-textarea']": "Source code"
+//     }
+//     for ([selector, text] of Object.entries(replaces)) {
+//         document.querySelector(selector).innerText = text
+//     }
+// }
 
 class ResultError {
     constructor(message, lineno) {
@@ -75,7 +79,14 @@ class ResultError {
     }
 
     get format() {
-        if (this.message.startsWith("?")) {
+        if (this.message.startsWith("$")) {
+            this.lineno = 0
+            if (this.message.charAt(1) == "?") {
+                return `（${this.message.substr(2)}）`
+            } else {
+                return `（${this.message.substr(1)}）`
+            }
+        } else if (this.message.startsWith("?")) {
             return `（${this.lineno} : ${this.message.substr(1)}）`
         } else {
             return `${this.lineno} : ${this.message}`
@@ -88,11 +99,12 @@ function expandMatch(match, baseString) {
     return baseString
 }
 function detectCode() {
-    content = document.getElementById("main-code-textarea").value.replaceAll(/ *([,=]) */g, "$1").replaceAll(/^ +/g, "").replaceAll(/#.*/g, "")
+    content = document.getElementById("main-code-textarea").value.replaceAll(/ *([,=]) */g, "$1").replaceAll(/^ +/g, "").replaceAll(/#.*/g, "").replaceAll(/\n{2,}/g, "\n")
     if (content.length == 0) {
         document.getElementById("result-code-textarea").value = ""
         return
     }
+    document.getElementById("result-code-textarea").value = "検出中です..."
     code = ""
     inQuote = null
     bracketCounter = 0
@@ -126,7 +138,7 @@ function detectCode() {
     }
     results = []
     eventCounter = {}
-
+    showVerbose = document.getElementById("result-show-verbose-input").checked
     for ([regex, message] of ERRORS) {
         for (match of code.matchAll(regex)) {
             bc = code.substr(0, match.index)
@@ -135,7 +147,11 @@ function detectCode() {
                 lineIndex = 0
             }
             line = lines[lineIndex]
-            results.push(new ResultError(expandMatch(match, message), line))
+            if (!showVerbose && (message.startsWith("?") || message.charAt(1) == "?")) {
+                continue
+            } else {
+                results.push(new ResultError(expandMatch(match, message), line))
+            }
         }
     }
     if (results.length > 0) {
